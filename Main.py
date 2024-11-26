248dc58742c90b38f5b5a3f99e633b78862a5273
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-11-26 18:16:17 -0300

    feat: finalizada implementação do registro e verificação do aquivo de history_ids

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
+from json import JSONDecodeError
+
 import discord
 from discord.ext import commands, tasks
 import requests
 from datetime import datetime
+import json
+import io
 
-from credetials import credetials
+from credentials import credentials
 
-DISCORD_CHANNEL_ID = credetials.get('DISCORD_CHANNEL_ID')
-TWITCH_USERNAME = credetials.get('TWITCH_USERNAME')
-TWITCH_CLIENT_SECRET = credetials.get('TWITCH_CLIENT_SECRET')
-TWITCH_CLIENT_ID = credetials.get('TWITCH_CLIENT_ID')
-TOKEN = credetials.get('TOKEN')
+DISCORD_CHANNEL_ID = credentials.get('DISCORD_CHANNEL_ID')
+TWITCH_USERNAME = credentials.get('TWITCH_USERNAME')
+TWITCH_CLIENT_SECRET = credentials.get('TWITCH_CLIENT_SECRET')
+TWITCH_CLIENT_ID = credentials.get('TWITCH_CLIENT_ID')
+DISCORD_TOKEN = credentials.get('DISCORD_TOKEN')
 
 # Configurar Intents do Discord
 intents = discord.Intents.default()
@@ -24,11 +28,27 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # Armazenar o ID da última live para evitar notificações repetidas
 last_stream_id = None
 
+# Arquivo que armazena os IDs enviados
+history_file = "history_ids.json"
+
+# Set de IDs já anunciados
+history_lives_annoucement = set()
+
 # Registra os logs
 def log_register(message):
     moment = datetime.now()
-    print(f"{moment} {message}\n")
+    print(f"{moment} {message}")
     open("logs.txt", "a").write(f"{moment} {message}\n")
+
+# Carrega os IDs dos lives já anunciados
+try:
+    with open(history_file, "r") as f:
+        history_lives_annoucement = set(json.load(f))
+except FileNotFoundError:
+    history_lives_annoucement = set()
+except JSONDecodeError:
+    log_register("JSONDecodeError: provavelmente o arquivo JSON está vazio.")
+    history_lives_annoucement = set()
 
 # Obter o Token de Acesso da Twitch
 def get_twitch_access_token():
@@ -66,7 +86,7 @@ def is_stream_live():
     return data and data[0].get("type") == "live"
 
 # Tarefa de Verificação Periódica
-@tasks.loop(minutes=1)
+@tasks.loop(minutes=0.5)
 async def check_stream_status():
     global last_stream_id
     channel = bot.get_channel(DISCORD_CHANNEL_ID)
@@ -78,16 +98,27 @@ async def check_stream_status():
     if is_stream_live():
         if current_stream_id:
             # Se a live está ao vivo e o ID é diferente do último, notifique
-            if current_stream_id != last_stream_id:
-                log_register(f"ID da Live atual: {current_stream_id}. ID Anterior: {last_stream_id}.")
-                last_stream_id = current_stream_id
+            # Verifica se de id foi persistido no arquivo history_ids.json
+            if current_stream_id not in history_lives_annoucement:
+                log_register(f"ID da Live atual: {current_stream_id}. O ID atual não estava nos IDs anteriores.")
+
+                # Adiciona o ID da live atual à lista de anunciados
+                history_lives_annoucement.add(current_stream_id)
+
+                # Salva a lista atualizada em disco
+                buffer = io.StringIO()
+                json.dump(list(history_lives_annoucement), buffer)
+                with open("history_ids.json", "w") as file:
+                    file.write(buffer.getvalue())
+
                 await channel.send(
                     f"@everyone O canal {TWITCH_USERNAME} está ao vivo! Assista aqui: https://www.twitch.tv/{TWITCH_USERNAME}")
                 log_register(f"Mensagem de aviso de Live enviada!")
+            else:
+                log_register("O ID atual já foi anunciado.")
         else:
-            # Se a live terminou, redefina last_stream_id para permitir nova notificação
-            log_register(f"Resetando last_stream_id. ID Atual: {current_stream_id}. ID Anterior: {last_stream_id}.")
-            last_stream_id = None
+            # ...
+            log_register(f"ID atual: {current_stream_id}.")
     else:
         print(f"O streamer {TWITCH_USERNAME} não está ao vivo.")
 
@@ -97,9 +128,8 @@ async def on_ready():
     # Inicia a tarefa de verificação
     check_stream_status.start()
 
-
 # Rodar o bot
 try:
-    bot.run(TOKEN)
+    bot.run(DISCORD_TOKEN)
 except Exception as e:
     log_register(f"Ocorreu um erro: {str(e)}")
