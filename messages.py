dbbbd22c3f476ccbfb082ff33b5204c4d630a544
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-01 16:45:26 -0300

    feat: mensagens atualizadas

--- messages.py
+++ messages.py
@@ -1,12 +1,12 @@
 messages = [
     "Tá rolando live!",
     "Bora? Já tá ON e só falta você no chat!",
-    "Vem que tá quente! Link no papo, só chegar.",
-    "{username} tá ON, diversão garantida ou seu dinheiro de volta (que é grátis mesmo :kek:).",
+    "Vem que tá quente! Link no papo, só chegar:",
+    "{username} tá ON, diversão garantida ou seu dinheiro de volta :kek:",
     "LIVE",
     "Bora pra resenha ao vivo? A galera tá chegando, e o hype tá real!",
     "O {username} abriu live! Cola aí:",
     "{username} está ao vivo! Assista aqui:",
-    "A live tá pegando fogo (literalmente? talvez não, mas vem conferir).",
+    "A live tá pegando fogo (literalmente? talvez não, mas vem conferir)",
     "Tá esperando o quê? Só falta você pra completar a zoeira no chat:"
 ]
